b902768fab3b352de4bc21a3da52961c81d1b796
Antônio Zilli Zanolli <antoniozzanolli@gmail.com>
2022-07-14 16:49:44 -0300

    Dapper Contrib

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ using Microsoft.Data.SqlClient;
 
 class Program
 {
-    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+    static void Main(string[] args)
+    {
+        ReadUsers();
+    }
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
     public static void ReadUsers()
     {
         using (var connection = new SqlConnection(CONNECTION_STRING))
